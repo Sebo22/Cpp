5fe1d6c6099d927d038937a7d21103c92420b241
Sebo22 <95843127+Sebo22@users.noreply.github.com>
2021-12-09 08:39:13 +0100

    Add files via upload

---
+++ Sebastian.Kolic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+int stranicaA=5;
+int stranicaB=7;
+int stranicaC=9;
+printf("Unesi;str\n");
+scanf("%d %d %d",&stranicaA,&stranicaB,&stranicaC);
+int volumen=stranicaA*stranicaB*stranicaC;
+printf("%d",volumen);
+}
