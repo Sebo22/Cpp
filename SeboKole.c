f5bc1a4ca4c58f1558701529c135700af25434a8
Sebo22 <95843127+Sebo22@users.noreply.github.com>
2021-12-16 09:32:06 +0100

    Add files via upload

---
+++ SeboKole.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int a;
+    int b;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    int p= (a*b)/2;
+    printf("površina trokuta je = %d\n",p);
+
+    return 0;
+}
