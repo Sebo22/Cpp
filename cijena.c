a98a1ea2d50d917e2fcd87ddabc1e7f77a79b693
Sebo22 <95843127+Sebo22@users.noreply.github.com>
2022-01-13 10:41:06 +0100

    Add files via upload

---
+++ cijena.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int cijena;
+    int sati;
+    int minute;
+
+    printf("Unesi cijenu proizvoda");
+    scanf("%d,&c");
+
+    printf("Unesi iznos minuta");
+    scanf("%d, &c");
+
+    printf("konacna cijena: %d",c-s-m);
+
+
+
+
+return 0;
+}
