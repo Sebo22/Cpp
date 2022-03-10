834165fe7f27a9c77eb7d86b408a45d209a5ac76
Sebo22 <95843127+Sebo22@users.noreply.github.com>
2022-03-10 07:32:32 +0100

    Add files via upload

---
+++ parniineparnibrojevi.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+
+{
+
+    int a;
+
+    printf("Unesi broj\n");
+    scanf("%d", &a);
+
+    if(a%2==1)
+
+     printf("Broj je neparan");
+
+
+    if(a%2==0)
+
+     printf("Broj je paran");
+
+
+    return 0;
+}
