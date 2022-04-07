b871cfac1256aa56eae6c3c450f16d419c6587ab
Sebo22 <95843127+Sebo22@users.noreply.github.com>
2022-04-07 08:09:35 +0200

    Add files via upload

---
+++ vrijeme.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+
+
+    float a,b,c,d=1;
+
+
+
+
+    while(d){
+
+    a=b;
+    b=c;
+    c=d;
+    scanf("%f", &d);
+    }
+    printf("razlika:%.2f\n",a-c);
+
+
+
+    return 0;
+
+
+
+
+}
