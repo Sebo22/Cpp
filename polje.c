09ed9d03b8825a4ff3bdba132578207122b09c1e
Sebo22 <95843127+Sebo22@users.noreply.github.com>
2022-05-12 08:43:10 +0200

    Add files via upload

---
+++ polje.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+int main ()
+
+{
+    int i;
+    int a[5]={16,-3,4,2,10};
+
+    for(i=4;i>=0;i--){
+
+    printf("%d\n",a[i]);
+
+    }
+
+    return 0;
+}
+
+
