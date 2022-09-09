08fa5c52d47e1a1aa7e9260c91baf68a2861f5b7
Sebo22 <95843127+Sebo22@users.noreply.github.com>
2022-09-09 11:12:20 +0200

    Add files via upload

---
+++ sajam2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+int n;
+int k;
+
+printf("unesi broj prijatelja \n");
+printf("unesi broj karata \n");
+scanf("%d %d",&n ,&k);
+
+if (n<=k){
+	printf("moci ces posjetiti sajam s sa svim prijateljima");
+}
+else {
+	printf("neces mocim posjetiti sajam s sa svim prijateljima");
+}
+
+
+
+
+}
