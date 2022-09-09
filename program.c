171d61d67fe94bd2de1a15fb3ab910b5d6ba365f
Sebo22 <95843127+Sebo22@users.noreply.github.com>
2022-09-09 10:22:41 +0200

    Add files via upload

---
+++ program.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main(){
+
+int x;
+
+printf("unesi kreditnu ocjenu x:");
+scanf("%d",&x);
+
+if (x<750){
+	printf("Ova osoba nema pristup KREDU!");
+}
+
+else {
+	printf("Ova osoba ima pristup KREDU!");
+}
+
+}
